d6d1f18c7239192bb8feacc5134ec55a0fe191c1
blackPie8 <stanashray@gmail.com>
2025-09-10 16:23:14 +0530

    [Ashray] ADD: Added camera movement with player

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform playerTransform;
+    public Vector3 offset;
+    public float smoothSpeed = 0.125f;
+    void LateUpdate()
+    {
+        Vector3 desiredPos = playerTransform.position + offset;
+        transform.position = desiredPos;
+        
+        // transform.LookAt(playerTransform);
+    }
+}
